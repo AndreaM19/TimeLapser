bfb383904a8b5eeef748f10544c9a2afb0c66bd1
Andrea <19andrea.m@gmail.com>
2015-07-23 15:18:08 +0200

    Added input data from comand line

--- time-lapser.py
+++ time-lapser.py
@@ -4,33 +4,34 @@ import time
 import picamera
 import os
 
-directory='/home/pi/projects/TimeLapser/test-2';
+# Time between two frames (second)
+frame_interval=eval(input('Intervallo di scatto (Secondi): '))
+# Video Hours
+video_hours=eval(input('Durata del video (ore): '))
+# Folder name - contains captured frames
+folder=input('Nome cartella in cui salvare le immagini: ')
+directory='/home/pi/projects/TimeLapser/%s' % folder;
 
-#Time between two frames (second)
-FRAME_PERIOD=20
-VIDEO_HOURS=1
-FRAMES=int(((60*60)/FRAME_PERIOD)*VIDEO_HOURS)
-
-#camera=picamera.PiCamera()
+frames=int(((60*60)/frame_interval)*video_hours)
 
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 # Function capture_frame
-def capture_frame(frame):
-  time.sleep(2)
-  camera.capture('test-2/frame%03d.jpg' % frame)
-  print('Frame saved in test-2/frame%03d.jpg' % frame)
+def capture_frame(frameId):
+    time.sleep(2)
+    camera.capture('%s/frame%03d.jpg' % (folder, frameId))
+    print('Frame saved in %s/frame%03d.jpg' % (folder, frameId))
 # End of function
 
 # Capture the images
 x=1
-for frame in range(FRAMES):
+for frameId in range(frames):
     # Note the time before the capture
     print('Capturing image %d...' % x)
     camera=picamera.PiCamera()
     x=x+1
-    capture_frame(frame)
+    capture_frame(frameId)
     camera.close()
     # Wait for the next capture
-    time.sleep(FRAME_PERIOD-2)
+    time.sleep(frame_interval-2)
